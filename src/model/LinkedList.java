b55776cbea6dfc8cd27f23e9b5c647524b070680
Juan Felipe Castillo Gomez <juanfelipe.castillogomez@gmail.com>
2022-03-17 22:13:27 -0500

    Implement class LinkedList and set

---
+++ src/model/LinkedList.java
@@ -0,0 +1,18 @@
+package model;
+
+public class LinkedList<T> {
+    private NodeH<K, V> head;
+    private int size;
+
+    public LinkedList{
+        head = null;
+        size = 0;
+    }
+
+    public NodeH<K, V> getHead() {
+        return head;
+    }
+
+
+
+}
