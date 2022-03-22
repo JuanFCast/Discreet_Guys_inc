7dbc1ae4e84206c30e1b602d96a3509ee39c86c0
Juan Felipe Castillo Gomez <juanfelipe.castillogomez@gmail.com>
2022-03-21 22:16:57 -0500

    LinkedList complete

--- src/model/LinkedList.java
+++ src/model/LinkedList.java
@@ -2,13 +2,79 @@ package model;
 
 public class LinkedList<T> {
     private NodeH<T> head;
+    private int size;
 
     public LinkedList(){
         head = null;
+        size = 0;
     }
 
     public NodeH<T> getHead() {
         return head;
     }
+    
+    public boolean isEmpty() {
+        return (head == null) ? true : false;
+    }
+    
+    public int size() {
+        return size;
+    }
+    
+    public void add(T t) {
+
+        NodeH<T> node = new NodeH<T>(t);
+
+
+        if (head==null){
+            head=node;
+        }else{
+            NodeH<T> aux=head;
+       
+            while (aux.getNext()!=null) {
+                aux=aux.getNext();
+            }
+    
+            aux.setNext(node);
+        }
+       
+
+        size += 1;
+    }
+    
+    public int indexOf(T t) {
+        return indexOf(t, head, 0);
+    }
+    
+    private int indexOf(T t, NodeH<T> temp, int contador) {
+        if (t.equals(temp.getValue())) {
+            return contador;
+
+        } else {
+            return indexOf(t, temp.getNext(), contador + 1);
+
+        }
+    }
+    
+    
+    public T get(int index) {
+
+        return get(index, head);
+
+    }
+    
+    
+    private T get(int index, NodeH<T> temp) {
+
+        if (index == 0) {
+            return temp.getValue();
+
+        } else {
+            return get(index - 1, temp.getNext());
+
+        }
+
+    }
+
 
 }
