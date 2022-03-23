e123718b7358d3a820cb007b64bb5a91a28e8c58
Jesus David Rodriguez Burbano <jesusdavidrodriguezburbano@gmail.com>
2022-03-23 08:05:02 -0500

    Queue and Stack are finished.

--- src/model/Queue.java
+++ src/model/Queue.java
@@ -2,26 +2,53 @@ package model;
 
 import model.Interface.IQueue;
 
+//Basado en el codigo del video: https://www.youtube.com/watch?v=V5hK2T898q8&list=WL&index=33
+
 public class Queue<T> implements IQueue<T>{
-	
-	public T element;
+
+	private int last;
+	private int size;
+	private T queue[];
+
+	public Queue(int m){
+		size = m;
+		last = -1;
+		queue = (T[]) new Object[size];
+	}
 
 	@Override
 	public boolean add(T element) {
-		// TODO Auto-generated method stub
-		return false;
+		if(last==size-1){
+			return false;
+		}else{
+			last++;
+			queue[last] = element;
+			return true;
+		}
 	}
 
 	@Override
 	public T poll() {
-		// TODO Auto-generated method stub
-		return null;
+		if(last==-1){
+			return null;
+		}else{
+			T aux = queue[0];
+			for(int i = 0; i < size-1; i++){
+				queue[i] = queue[i+1];
+			}
+			queue[last] = null;
+			last--;
+			return aux;
+		}
 	}
 
 	@Override
 	public T peek() {
-		// TODO Auto-generated method stub
-		return null;
+		if(last==-1){
+			return null
+		}else{
+			return queue[0];
+		}
 	}
 
 }
--- src/model/Stack.java
+++ src/model/Stack.java
@@ -1,5 +1,44 @@
 package model;
 
-public class Stack {
+//Basado en el codigo del video: https://www.youtube.com/watch?v=R0mTivTmFr8&list=WL&index=33
+
+public class Stack<T> {
+
+    private int size;
+    private int current;
+    private T stack[];
+
+    public Stack(int m){
+        size = m;
+        current = 0;
+        stack = (T[]) new Object[size];
+    }
+
+    private void push(T e){
+        if(current<size){
+            stack[current++] = e;
+        }
+    }
+
+    private T pop(){
+        if(!empty()){
+            return stack[--current];
+        }else{
+            return null;
+        }
+    }
+
+    private boolean empty(){
+        return current==0;
+    }
+
+    private T peek(){
+        if(empty()){
+            return null;
+        }else{
+            int aux = current;
+            return stack[--aux];
+        }
+    }
 
 }
